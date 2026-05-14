cac43a60067aecb502be350f4222c3d95215cec2
penyelamatbumi <inihanip7@gmail.com>
2026-05-14 11:19:56 +0700

    menambahkan fungsi pop pada stack

--- stackUsingLinkedList.cpp/stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp/stackUsingLinkedList.cpp
@@ -36,3 +36,16 @@ public:
         return value;
     }
 
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty." << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Popped value: " << top->data << endl;
+    }
+
+    
