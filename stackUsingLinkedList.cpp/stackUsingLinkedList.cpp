8fe7382ebeec920e3a8e617818c6f0360d0d8c2b
penyelamatbumi <inihanip7@gmail.com>
2026-05-14 11:35:51 +0700

    menambahkan valisdasi stack kosong sebelum peek

--- stackUsingLinkedList.cpp/stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp/stackUsingLinkedList.cpp
@@ -106,4 +106,15 @@ int main()
                 cout << "Stack is empty. Cannot pop." << endl;
             }
             break;
-        
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek();
+            }
+            else
+            {
+                cout << "Stack is empty.No top value." << endl;
+            }
+            break;
+
+    
